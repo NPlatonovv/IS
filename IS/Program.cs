08df9ab7e6b4dec477b5030ce1cc9dccd865df5f
Nikita <pl_an777@list.ru>
2025-09-24 16:34:11 +0700

    1

--- IS/Program.cs
+++ IS/Program.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace IS
 {
@@ -25,7 +26,45 @@ namespace IS
                 }
             }
 
-            FilePrinter.PrintFiles(files);
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("1. Вывести все файлы");
+                Console.WriteLine("2. Добавить файл");
+                Console.WriteLine("3. Удалить файл");
+                Console.WriteLine("4. Выход");
+                Console.Write("Выберите действие: ");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        FilePrinter.PrintFiles(files);
+                        break;
+                    case "2":
+                        Console.WriteLine("Введите данные файла");
+                        var line = Console.ReadLine();
+                        InputFile item = FileParser.ParseLine(line);
+                        if (item != null)
+                        {
+                            files.Add(item);
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Введите имя файла");
+                        var fileName = Console.ReadLine();
+                        files.RemoveAll(s => s.Name == fileName);
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Неверный выбор!");
+                        break;
+                }
+                Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+                Console.ReadKey();
+            }
         }
     }
 }
